3599d97e3946670dceba972fedb45b3804cc645d
kritig0206 <kritig0206@gmail.com>
2026-06-09 11:36:20 +0000

    new file added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" ; // treats all js code as newer version of code
+
+//alert(3 + 3) node.js does not allow this but browser does
+ console.log(3+3)
+
+ // ECMA is an organisation to follow standards of js (orignal)
+ // MDN can be used 
+
+ let name="Hitesh" // string ="" or ''
+ let age = 18 // number = 2 to power 53
+// bigint for big numbers 
+let isLoggedin="true";// boolean
+// null is a standalone value or datatype
+// undefined no value assigned
+// symbol used to find uniqueness
+
+
+// object 
+console.log(typeof "Kriti") // string
+console.log(typeof age); // number
+console.log(typeof null) // output -object
+console.log(typeof undefined)// output - undefined
